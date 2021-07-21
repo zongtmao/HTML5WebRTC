474af1c2790eb384ace8665606d1881d7a3e8b5e
zongtmao <maozongtai@163.com>
2021-07-22 00:34:30 +0800

    feat🚩：init project

---
+++ src/views/microphone/UseMicrophone.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="use-microphone-wrap">
+        使用麦克风demo
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UseMicrophone"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
